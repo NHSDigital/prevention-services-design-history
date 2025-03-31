19a0edc4f4c66b17294e9b3ec3b359f6b4108f8d
LEE GATHERCOLE <lee.gathercole3@nhs.net>
2025-03-31 17:11:34 +0100

    Update linting fixes

--- eleventy.config.js
+++ eleventy.config.js
@@ -65,13 +65,13 @@ module.exports = function (eleventyConfig) {
     return collection.getFilteredByGlob('app/posts/manage-breast-screening/**/*.md')
   })
 
-  eleventyConfig.addCollection("manage-your-screening", (collection) => {
-    return collection.getFilteredByGlob("app/posts/manage-your-screening/**/*.md")
-  });
-
   eleventyConfig.addCollection("bowel-screening", (collection) => {
     return collection.getFilteredByGlob("app/posts/bowel-screening/**/*.md")
-  });
+  })
+
+  eleventyConfig.addCollection("manage-your-screening", (collection) => {
+    return collection.getFilteredByGlob("app/posts/manage-your-screening/**/*.md")
+  })
 
   eleventyConfig.addCollection('hpv-self-sampling', collection => {
     return collection.getFilteredByGlob('app/posts/hpv-self-sampling/**/*.md')
