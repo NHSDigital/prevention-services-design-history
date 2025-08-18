cb34aee776d43fb35036482d2be5cdb18c365196
Frankie Roberto <frankie@frankieroberto.com>
2025-08-18 22:46:14 +0100

    Update config to use NHS Eleventy plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,63 +1,41 @@
-import { govukEleventyPlugin } from '@x-govuk/govuk-eleventy-plugin'
+import { nhsukEleventyPlugin } from '@x-govuk/nhsuk-eleventy-plugin'
 
 export default function(eleventyConfig) {
 
-  // Options to customise the appearance of your design history
-  // https://x-govuk.github.io/govuk-eleventy-plugin/options/
-  eleventyConfig.addPlugin(govukEleventyPlugin, {
-    stylesheets: [
-      '/styles/application.css'
-    ],
-    themeColour: '#005eb8',
-    icons: {
-      mask: 'mask-icon.svg',
-      shortcut: 'favicon.ico',
-      touch: 'apple-touch-icon.png'
+  eleventyConfig.addPlugin(nhsukEleventyPlugin, {
+    templates: {
+      feed: true,
+      tags: true,
+      searchIndex: true
     },
-    opengraphImageUrl: 'opengraph-image.png',
-    headingPermalinks: true,
-    titleSuffix: 'NHS Digital prevention services design history',
     header: {
-      logotype: {
-        html: `<svg class="app-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 32" height="40" aria-hidden="true" focusable="false"><path fill="currentColor" d="M80 0v32H0V0h80ZM69 2.2c-5.8 0-11.6 2-11.6 8.8 0 7.4 10.2 5.8 10.2 10 0 2.6-3.4 3-5.6 3-2.2 0-5-.6-6.4-1.4L54 28c2.2.8 5.4 1.4 8 1.4 6.2 0 12.8-1.8 12.8-9 0-7.8-10.2-6.6-10.2-10.2 0-2.2 2.2-2.6 5-2.6 2.6 0 4.4.6 5.8 1.2L77 3.4c-1.8-.8-4.8-1.2-8-1.2ZM16.6 3H7.8L2.2 29h6.6l3.6-18h.2L18 29h8.6l5.6-26h-6.6L22 21h-.2L16.6 3Zm25.2 0h-7.2l-5.2 26h6.8l2.4-11.2h8.2L44.6 29h7L57 3h-7l-2.2 9.8h-8l2-9.8Z"/></svg>`,
+      service: {
+        text: "Digital prevention services design history",
+        href: "/"
       },
-      productName: 'Digital prevention services design history',
-      organisationName: "NHS Digital prevention services design history",
       search: {
-        indexPath: '/search.json',
-        sitemapPath: '/sitemap'
+        action: "/search/",
       }
     },
     footer: {
-      logo: false,
       meta: {
         items: [
           {
-            href: "/posts/feed.xml",
-            text: "Subscribe to feed"
+            text: "Sitemap",
+            href: "/sitemap"
           },
           {
-            href: "https://github.com/NHSDigital/prevention-services-design-history",
-            text: "Github source"
+            text: "Subscribe to feed",
+            href: "/feed.xml"
           },
           {
-            href: "/sitemap",
-            text: "Sitemap"
-          },
-          {
-            href: "/tags",
-            text: "Tags"
+            text: "Tags",
+            href: "/tags"
           }
         ],
-        html: `<p class="app-footer--text">Made by the NHS with <a class="govuk-footer__link" href="https://github.com/x-govuk/nhsuk-design-history-template">X-GOVUK NHS design history template</a></p>
-        <p class="app-footer--text">All data and personal information shown in prototypes are fictional and for demonstration purposes only</p>`
-      },
-      copyright: {
-        text: '© NHS England'
-      },
-      contentLicence: false
+        text: "All data and personal information shown in prototypes are fictional and for demonstration purposes only."
+      }
     },
-    feedUrl: 'posts/feed.xml',
     url:
       process.env.GITHUB_ACTIONS &&
       'https://design-history.prevention-services.nhs.uk/'
@@ -188,9 +166,7 @@ export default function(eleventyConfig) {
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
     dir: {
-      input: 'app',
-      layouts: '_layouts',
-      includes: '_components'
+      input: 'app'
     }
   }
 }
